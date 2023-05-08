b1c2d5b266084d4e79853863dfedb0e6b9e69ade
prvladko <prvladko@gmail.com>
2023-05-08 07:50:19 +0300

    make a prompt template

--- youtube_script_generator/app.py
+++ youtube_script_generator/app.py
@@ -4,16 +4,25 @@ from apikey import apikey
 
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 # App framework
 os.environ['OPENAI)API_KEY'] = apikey
 st.title('🦜🔗 YouTube GPT Creator')
 prompt = st.text_input('Plug in your prompt here: ')
 
+# Prompt templates
+title_template = PromptTemplate(
+    input_variables = ['topic'],
+    template='write me a youtube video title about {topic}'
+)
+
 # LLMs
 llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
 
 # Show stuff to the screen if there's a prompt
 if prompt:
-    response = llm(prompt)
+    response = title_chain.run(topic=prompt)
     st.write(response)
