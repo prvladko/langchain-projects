79c886e78d65fb8f7dbfb211ea5e5471e915311e
prvladko <prvladko@gmail.com>
2023-05-08 07:34:10 +0300

    create instance of OpenAI server

--- youtube_script_generator/app.py
+++ youtube_script_generator/app.py
@@ -9,3 +9,11 @@ from langchain.llms import OpenAI
 os.environ['OPENAI)API_KEY'] = apikey
 st.title('🦜🔗 YouTube GPT Creator')
 prompt = st.text_input('Plug in your prompt here: ')
+
+# LLMs
+llm = OpenAI(temperature=0.9)
+
+# Show stuff to the screen if there's a prompt
+if prompt:
+    response = llm(prompt)
+    st.write(response)
